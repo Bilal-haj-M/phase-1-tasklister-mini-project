66c6c2c143a25dc64c3ae12cfae0cf247cc075bc
Bilal-hajM <bblal1003@gmail.com>
2023-04-25 03:38:16 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const newTaskForm = document.querySelector("#create-task-form");
+const newTaskInput = document.querySelector("#new-task-description");
+const taskList = document.querySelector("#tasks");
+
+newTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+const NewTask = document.createElement("li");
+NewTask.innerText = newTaskInput.value;
+taskList.appendChild(NewTask);
+
+newTaskForm.reset()
+
+})
+
+})
